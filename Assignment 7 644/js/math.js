e1094224548b7734bc82fffc6e0eb57c35930c9e
misslavada <56700239+misslavada@users.noreply.github.com>
2020-04-14 19:29:22 -0700

    Add files via upload

---
+++ Assignment 7 644/js/math.js
@@ -0,0 +1,17 @@
+/*eslint-env browser*/
+
+//STEP 1
+// let step1 = prompt('Enter a negative or positive number:');
+// console.log(Math.abs(step1));
+//STEP 2
+// let step2 = prompt('Enter a number with a decimal:');
+// console.log(Math.ceil(step2));
+//STEP 3
+// let step3 = prompt('Enter a number with a decimal:');
+// console.log(Math.floor(step3));
+//STEP 4
+let step4 = prompt('Choose 5 numbers, separated by commas (ex: 1, 2, 3, 4, 5):');
+console.log(Math.min(...step4));
+//STEP 5
+// let step5 = prompt('Enter any number:');
+// console.log(Math.sqrt(step5));
